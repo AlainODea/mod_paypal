471f906316a6b6287627fdf551267f1f9f665ef8
Alain O'Dea <alain.odea@gmail.com>
2012-12-01 18:51:25 -0330

    Apply APLv2

--- mod_paypal.erl
+++ mod_paypal.erl
@@ -1,10 +1,7 @@
 %% @author Alain O'Dea <alain.odea@gmail.com>
-%% @copyright 2011 Alain O'Dea
 %% @date 2011-01-21
 %% @doc PayPal IPN Module.  Supports integration with Instant Payment Notification in Zotonic.
 
-%% Copyright 2011 Alain O'Dea
-%%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
 %% You may obtain a copy of the License at
